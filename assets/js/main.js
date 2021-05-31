2798c3a54bc5fb844798d60b5ec0949cdfa53f14
Nhat Duy <huynhnhatduy2112@gmail.com>
2021-05-31 08:42:35 +0700

    qr code

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,32 +12,59 @@ closeMenu.addEventListener("click", () => {
   navMenu.classList.remove("show");
 });
 
-// Remove menu 
-const navLink =document.querySelectorAll(".nav_link");
+// Remove menu
+const navLink = document.querySelectorAll(".nav_link");
 
-function linkAction(){
-    navMenu.classList.remove("show");
+function linkAction() {
+  navMenu.classList.remove("show");
 }
-navLink.forEach(link=>link.addEventListener("click", linkAction));
+navLink.forEach((link) => link.addEventListener("click", linkAction));
 
 // Scroll section
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener('scroll',scrollActive)
+window.addEventListener("scroll", scrollActive);
 
-function scrollActive(){
-  const scrollY =window.pageYOffset;
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-  sections.forEach(curren=>{
+  sections.forEach((curren) => {
     const sectionHeight = curren.offsetHeight;
-    const sectionTop = curren.offsetTop -50;
-    sectionId = curren.getAttribute('id');
+    const sectionTop = curren.offsetTop - 50;
+    sectionId = curren.getAttribute("id");
 
-    if(scrollY>sectionTop&&scrollY<= sectionTop+sectionHeight){
-        document.querySelector('.nav__menu a[href*='+sectionId+']').classList.add("active")
-    }
-    else{
-        document.querySelector('.nav__menu a[href*='+sectionId+']').classList.remove("active")
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
     }
   });
 }
+
+function onSumit() {
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwCt8Nx7CECpb0LhqhJIQFQasTPCLyWTATaXsc3rYc3ZScB2FwX-Bv44hq_BNznuO8I_A/exec";
+  const form = document.forms["google-sheet"];
+  // console.log(FormData(form));
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => alert("You have successfully submitted."))
+      .catch((error) => console.error("Error!", error.message));
+  });
+}
+
+ // QR CODE
+ var qrcode = new QRCode("id-qrcode", {
+  text: "https://github.com/huynhnhatduy108",
+  width: 180,
+  height: 180,
+  colorDark: "#000000",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H,
+});
